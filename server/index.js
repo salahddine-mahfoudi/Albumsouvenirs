8ad9e7bdfb038490da75a72f42936a87dd7e64c0
salaheddine <salaheddine.mahfoudi@gmail.com>
2021-01-10 21:37:46 +0100

    commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/posts', postRoutes); 
+
+// const CONNECTION_URL = 'mongodb+srv://Salah_Eddie11:Salah_Eddie11mdp11@cluster0.gjj44.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
